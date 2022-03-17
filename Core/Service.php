3a4f034c10692c18c786b8b79f3f3c66020373ed
wkdwilliams <31390496+wkdwilliams@users.noreply.github.com>
2022-03-17 19:42:11 +0700

    Change variable type

--- Core/Service.php
+++ Core/Service.php
@@ -19,11 +19,11 @@ abstract class Service
     }
 
     /**
-     * @param string $id
+     * @param int $id
      * 
      * @return Entity
      */
-    public function getResourceById(string $id): Entity
+    public function getResourceById(int $id): Entity
     {
         return $this->repository->findById($id)->entity();
     }
